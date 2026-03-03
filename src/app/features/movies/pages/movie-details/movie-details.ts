b1a5a068b128b36d0601f96f27c069e2823ec455
jonathancesar <jonathancesar1995@gmail.com>
2026-03-03 20:23:15 -0300

    Detalhes dos filmes

--- src/app/features/movies/pages/movie-details/movie-details.ts
+++ src/app/features/movies/pages/movie-details/movie-details.ts
@@ -1,60 +1,64 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  linkedSignal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { MoviesApi } from '../services/movie-api';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-details',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.css',
 })
 export class MovieDetails {
-  reviewsCount = 5;
-  // Apenas uma array de 5 elementos para o @for loop
-  stars = new Array(5);
+  private readonly _moviesApi = inject(MoviesApi);
+
+  readonly BASE_URL = 'http://localhost:3000';
+
+  id = input.required<number>();
+
+  movieDetailsResource = rxResource({
+    params: () => this.id(),
+    stream: ({ params }) => this._moviesApi.getMovieDetails(+params), //colocando o + ele converte para number
+  });
+
+  movieDetails = linkedSignal(() => {
+    const ERROR_ON_RESPONSE = !!this.movieDetailsResource.error();
+    if (ERROR_ON_RESPONSE) return undefined;
+
+    return this.movieDetailsResource.value();
+  });
 
   // Sinais para controle de estado
-  isFavorite: WritableSignal<boolean> = signal(false);
-  currentRating: WritableSignal<number> = signal(4); // Inicia com 4 estrelas preenchidas
+  isFavorite = signal(false);
+  currentRating = signal<number | undefined>(undefined); // Inicia com 4 estrelas preenchidas
 
-  constructor() {}
+  starsStatusField = computed(() => {
+    const ratting = this.currentRating() ?? 0;
 
-  /**
-   * Verifica se a estrela em um dado índice deve estar preenchida (roxa).
-   * Substitui o [ngClass] no template.
-   */
-  isStarFilled(index: number): boolean {
-    // O índice (base 0) deve ser menor que o rating (base 1) para ser preenchido
-    return index < this.currentRating();
-  }
+    const boolArray = [0, 1, 2, 3, 4].map((index) => index < ratting);
 
-  /**
-   * Alterna o estado de favorito do filme.
-   */
+    return boolArray;
+  });
+
+  // Alterna o estado de favorito do filme.
   toggleFavorite() {
     this.isFavorite.update((value) => !value);
     console.log(`Filme agora é favorito: ${this.isFavorite()}`);
   }
 
-  /**
-   * Define o rating do filme baseado no clique na estrela.
-   */
-  setRating(event: MouseEvent) {
-    const target = event.target as SVGElement;
-
-    // Encontra o elemento SVG que tem o data-rating
-    const ratingElement = target.closest('svg');
-
-    if (ratingElement) {
-      const newRating = parseInt(ratingElement.getAttribute('data-rating') || '0', 10);
-
-      // Lógica de toggle: se clicar na estrela atual, zera. Senão, define o novo rating.
-      if (newRating === this.currentRating()) {
-        this.currentRating.set(0);
-      } else {
-        this.currentRating.set(newRating);
-      }
-
-      console.log(`Nova avaliação definida: ${this.currentRating()}`);
-      // Lógica de backend iria aqui
+  updateRating(newRating: number) {
+    if (newRating === this.currentRating()) {
+      this.currentRating.set(0);
+    } else {
+      this.currentRating.set(newRating);
     }
   }
 }
